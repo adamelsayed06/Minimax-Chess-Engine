36d5f523e74388ef94d12acc184efd6dac09907c
Adam Elsayed <adamelsayyed06@gmail.com>
2026-04-27 03:13:45 -0400

    search init / iteraiton logic

--- main.go
+++ main.go
@@ -7,9 +7,23 @@ import (
 	"github.com/corentings/chess/v2"
 )
 
-func getBestMove(game *chess.Game) chess.Move {
+func search(move chess.Move) int {
+	return 0
+}
+
+func getBestMove(game *chess.Game, depth int) chess.Move {
 	bestScore := math.Inf(-1)
+	var bestMove *chess.Move
 	validMoves := game.ValidMoves()
+
+	for _, move := range validMoves {
+		// _ indicates unused since we dont care about idx and go forces enumeration
+		
+		final_position_score_after_move = search(move)
+		// get final elo of playing said move
+	}
+
+
 
 
 }
@@ -21,7 +35,7 @@ func main() {
 	for game.Outcome() == chess.NoOutcome {
 		// select a random move
 		// moves := game.ValidMoves()
-		move := getBestMove(game)
+		move := getBestMove(game, 5)
 		if err := game.Move(&move, nil); err != nil {
 			panic(err) // Should not happen with valid moves
 		}
