0db1e5af9006030fb8ac847f08dbde0b8c6691cb
Adam Elsayed <adamelsayyed06@gmail.com>
2026-04-26 20:37:58 -0400

    draw fix deprecated

--- main.go
+++ main.go
@@ -1,29 +1,43 @@
 package main
 
-import(
+import (
+	"image/color"
+	"log"
+
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 /*
 
-*/
+ */
 
-type Game struct {}
+type Game struct {
+	x float32
+	y float32
+}
 
 func (g* Game) Update() error {
 	return nil
 }
 
-func (g* Game) Draw(screen *ebiten.Image) error {
-	return nil;
+func (g* Game) Draw(screen *ebiten.Image) {
+	vector.FillRect(screen, g.x, g.y, 40, 20, nil, false)
 }
 
 func (g* Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	// outsideWith, Height provided by engine (parameters)
 	// returns int, int
-	return 320, 460
+	return 320, 240
 }
 
 func main() {
+	game := &Game{};
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("My Chess Engine")
+
+	if err := ebiten.RunGame(game); err != nil{
+		log.Fatal(err)
+	} 
 
 }
